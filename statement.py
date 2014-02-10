26df7ac83814166947ccd94ffa5940b2ffe282e7
andruli <andruli@norma.local>
2014-02-09 21:40:11 -0300

    1) Agregados archivos de prueba en examples/programs/ 2) Agregado el utils/__init__.py para que utils pueda ser usado como un modulo 3) statements.py: Ahora los statments nos dicen las variables a definir para poder compilarse. Falta implementar la compilación.

--- statement.py
+++ statement.py
@@ -1,15 +1,15 @@
 class Statement (object):
+    def vars (self):
+        """
+        Free variables that need to be setted before the code can be compiled.
+        Returns: set of strings
+        """
+        raise NotImplementedError ()
     def compile (self, values):
         """
         Compiles a statment to machine code, replace vars with actual values
         stored on the input values.
         Returns: hex code.
-        """
-        raise NotImplementedError ()
-    def vars (self):
-        """
-        Free variables that need to be setted before the code can be compiled.
-        Returns: list of var names
         """
         raise NotImplementedError ()
     def __str__ (self):
@@ -28,6 +28,9 @@ class PhaseDecl (Statement):
                (self.number, "".join ([" %d" % x for x in self.ph_list]))
 
 class For (Statement):
+    icode = [0, 4]
+    def vars (self):
+        return set.union (*[x.vars () for x in self.statements])
     def __init__ (self, times, statements, phase_shift=False):
         self.times       = times
         self.statements  = statements
@@ -39,19 +42,30 @@ class For (Statement):
                self.__indent__ (il) + "endloop"
 
 class Acquire (Statement):
+    icode = 1
+    def vars (self):
+        return set ()
     def __pp__ (self, il):
         return self.__indent__ (il) + "acquire"
+    def compile (self, values):
+        return '0x00'
 
 class Pulse (Statement):
+    icode = 2
     def __init__ (self, pulse, phase):
         self.pulse = pulse
         self.phase = phase
+    def vars (self):
+        return set (['p%d' % self.pulse])
     def __pp__ (self, il):
         return self.__indent__ (il) + "p%d (ph%d)" % (self.pulse, self.phase)
 
 class Delay (Statement):
+    icode = 3
     def __init__ (self, delay):
         self.delay = delay
+    def vars (self):
+        return set (["d%d" % self.delay])
     def __pp__ (self, il):
         return self.__indent__ (il) + "d%d" % (self.delay)
 
@@ -59,6 +73,8 @@ class Program (object):
     def __init__ (self, phases, program):
         self.phases   = phases
         self.program  = program
+    def vars (self):
+        return set.union (*[x.vars () for x in self.program])
     def __str__ (self):
         return "".join (["phases:\n"] + [x.__pp__ (1) + '\n' for x in self.phases] +\
                ["program:"] + ['\n' + x.__pp__ (1) for x in self.program])
