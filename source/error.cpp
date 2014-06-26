59cebd6c01910708e7affd5bdc549f606d979aa0
andruli <andresb9163@gmail.com>
2014-06-26 14:54:04 -0300

    Nueva version de tipos

--- source/error.cpp
+++ source/error.cpp
@@ -3,29 +3,30 @@
 #include <stdlib.h>
 #include "./error.h"
 
-char *error_mensaje(int codigo) {
-    char *mensaje = NULL;
-    switch (codigo) {
-        case -EFREQMAX: mensaje = MEFREQMAX; break;
-        case -EFREQMIN: mensaje = MEFREQMIN; break;
-        case -EFASEMAX: mensaje = MEFASEMAX; break;
-        case -EFASEMIN: mensaje = MEFASEMIN; break;
-        case -EPOSINUM: mensaje = MEPOSINUM; break;
-        case -EFAILURE: mensaje = MEFAILURE; break;
-        case -ENOCOMANDO: mensaje = MENOCOMANDO; break;
-        case -ENOVALLINEA: mensaje = MENOVALLINEA; break;
-        case -ENONUMLINEA: mensaje = MENONUMLINEA; break;
-        case -ENOVALPATRON: mensaje = MENOVALPATRON; break;
-        case -ENOPATRON: mensaje = MENOPATRON; break;
-        case -ENOVALDATO: mensaje = MENOVALDATO; break;
-        case -ENODATO: mensaje = MENODATO; break;
-        case -ENOVALTIEMPO: mensaje = MENOVALTIEMPO; break;
-        case -ENOTIEMPO: mensaje = MENOTIEMPO; break;
-        case -EMAXLAZOS: mensaje = MEMAXLAZOS; break;
-        case -ENOARCHIVO: mensaje = MENOARCHIVO; break;
-        case -ESINTAXIS: mensaje = MESINTAXIS; break;
-        case -ESEMANTICO: mensaje = MESEMANTICO; break;
-        default: mensaje = MEDEFAULT;
-    }
-    return mensaje;
+char error_message_map[][] = {
+    "Se ha excedido el rango de frecuencia maxima.",
+    "Se ha excedido el rango de frecuencia minima.",
+    "Se ha excedido el rango de fase maxima.",
+    "Se ha excedido el rango de fase minima.",
+    "Se ha ingresado una posicion no valida.",
+    "Se ha producido un fallo general en el sistema.",
+    "No se ha ingresado un comando válido.",
+    "No se pudo encontrar el numero de linea.",
+    "El formato del numero de linea no es correcto.",
+    "El campo 'patron' no es de tipo numérico.",
+    "El campo 'patron' esta fuera del rango establecido.",
+    "El campo 'dato' no es de tipo numérico.",
+    "El campo 'dato' esta fuera del rango establecido.",
+    "El campo 'demora' no es de tipo numérico.",
+    "El campo 'demora' esta fuera del rango establecido.",
+    "Se ha excedido el numero de lazos anidados posibles.",
+    "Se intento acceder un archivo invalido.",
+    "Se detecto un error sintactico en el programa.",
+    "Se ejecuto una sentencia de lazo sin un retorno de lazo.",
+    "Se detecto un error semantico en el programa.",
+    "Ha ocurrido un error desconocido"
+}
+
+char *error_mensaje(r_mod_error codigo error_code) {
+    return error_message_map[error_code]
 }
--- source/tipos.h
+++ source/tipos.h
@@ -1,9 +1,11 @@
+// Copyright 2014 FaMAF
+
 #ifndef TIPOS_H
 #define TIPOS_H
 
 /** Define los tipos basicos **/
 
 typedef unsigned char byte;
-typedef	unsigned int  word;
+typedef unsigned int  word;
 
-#endif // TIPOS_H
+#endif  // TIPOS_H
